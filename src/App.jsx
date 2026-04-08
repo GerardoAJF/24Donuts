a8f5fefd0b638b15f174bb9b0b41cfdfb4daa406
Saez-J <20210067@ricaldone.edu.sv>
2026-04-08 09:55:20 -0600

    Componentes de Boton y Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import InputCustom from './components/Input/Input.jsx';
+import BotonPrimario from './components/Boton/Boton.jsx';
 
 function App() {
+  
   return (
-    <>
-    </>
-  )
+    <></>
+  );
 }
 
 export default App
