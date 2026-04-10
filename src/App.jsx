b094c769a6fdbaaffd7c3fe74ecd716a1c051168
Saez-J <20210067@ricaldone.edu.sv>
2026-04-09 22:42:52 -0600

    App limpio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import './App.css'
-import RegistrateForm from './components/Registerform/RegisterForm';
 
 function App() {
   return (
-    <div className="App">
-      {/* Simplemente llamamos al componente que creamos */}
-      <RegistrateForm />
-    </div>
+    <></>
   );
 }
 
