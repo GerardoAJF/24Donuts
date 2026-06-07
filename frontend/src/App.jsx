b443a5995d9d4c71f35deb477f4fb210b41a57c8
Saez-J <20210067@ricaldone.edu.sv>
2026-06-07 00:16:42 -0600

    Proteccion de rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,26 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginRouter from "./router/LoginRouter.jsx";
 import AdminRouter from "./router/AdminRouter.jsx";
 import PublicRouter from "./router/PublicRouter.jsx";
+import PrivateRoute from "./router/PrivateRouter.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import "./App.css";
-
+ 
 const App = () => {
     return (
         <BrowserRouter>
             <AuthProvider>
                 <Routes>
                     <Route path="/auth/*" element={<LoginRouter />} />
-                    <Route path="/admin/*" element={<AdminRouter />} />
+ 
+                    <Route element={<PrivateRoute />}>
+                        <Route path="/admin/*" element={<AdminRouter />} />
+                    </Route>
+ 
                     <Route path="/*" element={<PublicRouter />} />
-                    <Route path="/" element={<Navigate to="/auth/registro-inicial" />} />
-                    <Route path="*" element={<Navigate to="/auth/registro-inicial" />} />
+ 
+                    <Route path="/" element={<Navigate to="/auth/login" replace />} />
+ 
+                    <Route path="*" element={<Navigate to="/auth/login" replace />} />
                 </Routes>
             </AuthProvider>
         </BrowserRouter>
